bd47cc10f1cf92d2dcc9b2f76ff250a442b94a4d
ChrisSun0731 <chris20090731@gmail.com>
2025-07-06 17:35:33 +0800

    no dark mode

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -9,10 +9,6 @@
           </q-avatar>
           成功高中代聯會
         </q-toolbar-title>
-        <div>
-          <q-btn flat dense icon="brightness_6" @click="toggleDark" />
-          <q-btn flat dense icon="fullscreen" @click="toggleFullscreen" />
-        </div>
       </q-toolbar>
     </q-header>
 
@@ -30,70 +26,22 @@
 </template>
 
 <script setup lang="ts">
-import { ref, onMounted } from 'vue';
+import { ref } from 'vue';
 import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
-import { Dark, LocalStorage } from 'quasar';
 
-const leftDrawerOpen = ref<boolean>(true);
+const leftDrawerOpen = ref(true);
 
 const linksList: EssentialLinkProps[] = [
-  {
-    title: '首頁',
-    caption: '',
-    icon: 'home',
-    link: '/',
-  },
-  {
-    title: '法規、公告與會議紀錄',
-    caption: '',
-    icon: 'class',
-    link: '/law',
-  },
-  {
-    title: '行政',
-    caption: '',
-    icon: 'people',
-    link: '/administration',
-  },
-  {
-    title: '歷史',
-    caption: '',
-    icon: 'history',
-    link: '/history',
-  },
-  {
-    title: '聯絡我們',
-    caption: '',
-    icon: 'mail',
-    link: '/contact',
-  },
+  { title: '首頁', icon: 'home', link: '/' },
+  { title: '法規、公告與會議紀錄', icon: 'class', link: '/law' },
+  { title: '行政', icon: 'people', link: '/administration' },
+  { title: '歷史', icon: 'history', link: '/history' },
+  { title: '聯絡我們', icon: 'mail', link: '/contact' },
 ];
 
-onMounted(() => {
-  const savedDarkMode = LocalStorage.getItem('dark');
-  if (savedDarkMode !== null) {
-    if (savedDarkMode === 'true') {
-      Dark.set(true);
-    } else {
-      Dark.set(false);
-    }
-  }
-});
-
-function toggleLeftDrawer(): void {
+function toggleLeftDrawer() {
   leftDrawerOpen.value = !leftDrawerOpen.value;
 }
-
-function toggleDark(): void {
-  Dark.toggle();
-  LocalStorage.set('dark', Dark.isActive);
-}
-
-function toggleFullscreen(): void {
-  if (document.fullscreenElement) {
-    void document.exitFullscreen();
-  } else {
-    void document.documentElement.requestFullscreen();
-  }
-}
 </script>
+
+<style scoped></style>
