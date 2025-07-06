7d5a71d765b96d6a22fbc54d06cbd4e05e212716
ChrisSun0731 <chris20090731@gmail.com>
2025-07-06 15:05:33 +0800

    homepage done

--- src/layouts/MainLayout.vue
+++ src/layouts/MainLayout.vue
@@ -2,40 +2,24 @@
   <q-layout view="lHh Lpr lFf">
     <q-header elevated>
       <q-toolbar>
-        <q-btn
-          flat
-          dense
-          round
-          icon="menu"
-          aria-label="Menu"
-          @click="toggleLeftDrawer"
-        />
-
+        <q-btn flat dense round icon="menu" aria-label="Menu" @click="toggleLeftDrawer" />
         <q-toolbar-title>
-          Quasar App
+          <q-avatar>
+            <img alt="favicon" src="icons/LOGO.ico" height="38" width="38" />
+          </q-avatar>
+          成功高中代聯會
         </q-toolbar-title>
-
-        <div>Quasar v{{ $q.version }}</div>
+        <div>
+          <q-btn flat dense icon="brightness_6" @click="toggleDark" />
+          <q-btn flat dense icon="fullscreen" @click="toggleFullscreen" />
+        </div>
       </q-toolbar>
     </q-header>
 
-    <q-drawer
-      v-model="leftDrawerOpen"
-      show-if-above
-      bordered
-    >
+    <q-drawer v-model="leftDrawerOpen" show-if-above bordered>
       <q-list>
-        <q-item-label
-          header
-        >
-          Essential Links
-        </q-item-label>
-
-        <EssentialLink
-          v-for="link in linksList"
-          :key="link.title"
-          v-bind="link"
-        />
+        <q-item-label header> 選單 </q-item-label>
+        <EssentialLink v-for="link in linksList" :key="link.title" v-bind="link" />
       </q-list>
     </q-drawer>
 
@@ -46,57 +30,70 @@
 </template>
 
 <script setup lang="ts">
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 import EssentialLink, { type EssentialLinkProps } from 'components/EssentialLink.vue';
+import { Dark, LocalStorage } from 'quasar';
+
+const leftDrawerOpen = ref<boolean>(true);
 
 const linksList: EssentialLinkProps[] = [
   {
-    title: 'Docs',
-    caption: 'quasar.dev',
-    icon: 'school',
-    link: 'https://quasar.dev'
+    title: '首頁',
+    caption: '',
+    icon: 'home',
+    link: '/',
   },
   {
-    title: 'Github',
-    caption: 'github.com/quasarframework',
-    icon: 'code',
-    link: 'https://github.com/quasarframework'
+    title: '法規',
+    caption: '',
+    icon: 'class',
+    link: '/law',
   },
   {
-    title: 'Discord Chat Channel',
-    caption: 'chat.quasar.dev',
-    icon: 'chat',
-    link: 'https://chat.quasar.dev'
+    title: '行政',
+    caption: '',
+    icon: 'people',
+    link: '/administration',
   },
   {
-    title: 'Forum',
-    caption: 'forum.quasar.dev',
-    icon: 'record_voice_over',
-    link: 'https://forum.quasar.dev'
+    title: '歷史',
+    caption: '',
+    icon: 'history',
+    link: '/history',
   },
   {
-    title: 'Twitter',
-    caption: '@quasarframework',
-    icon: 'rss_feed',
-    link: 'https://twitter.quasar.dev'
+    title: '聯絡我們',
+    caption: '',
+    icon: 'mail',
+    link: '/contact',
   },
-  {
-    title: 'Facebook',
-    caption: '@QuasarFramework',
-    icon: 'public',
-    link: 'https://facebook.quasar.dev'
-  },
-  {
-    title: 'Quasar Awesome',
-    caption: 'Community Quasar projects',
-    icon: 'favorite',
-    link: 'https://awesome.quasar.dev'
-  }
 ];
 
-const leftDrawerOpen = ref(false);
+onMounted(() => {
+  const savedDarkMode = LocalStorage.getItem('dark');
+  if (savedDarkMode !== null) {
+    if (savedDarkMode === 'true') {
+      Dark.set(true);
+    } else {
+      Dark.set(false);
+    }
+  }
+});
 
-function toggleLeftDrawer () {
+function toggleLeftDrawer(): void {
   leftDrawerOpen.value = !leftDrawerOpen.value;
+}
+
+function toggleDark(): void {
+  Dark.toggle();
+  LocalStorage.set('dark', Dark.isActive);
+}
+
+function toggleFullscreen(): void {
+  if (document.fullscreenElement) {
+    void document.exitFullscreen();
+  } else {
+    void document.documentElement.requestFullscreen();
+  }
 }
 </script>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,43 +1,60 @@
 <template>
-  <q-page class="row items-center justify-evenly">
-    <example-component
-      title="Example component"
-      active
-      :todos="todos"
-      :meta="meta"
-    ></example-component>
+  <q-page class="row items-center justify-center">
+    <q-card
+      flat
+      bordered
+      class="q-pa-xl q-mx-auto shadow-2 bg-white"
+      style="max-width: 800px; width: 100%"
+    >
+      <q-card-section class="column items-center">
+        <q-img
+          src="/icons/LOGO.ico"
+          alt="cklogo"
+          style="max-width: 150px; max-height: 150px"
+          fit="contain"
+        />
+        <div class="text-h5 text-center q-mt-md">
+          <strong>臺北市立成功高級中學代班級代表聯席會</strong>
+        </div>
+      </q-card-section>
+
+      <q-separator spaced />
+
+      <q-card-section class="q-pa-md">
+        <div class="text-h6 text-center q-mb-md"><strong>關於代聯會</strong></div>
+        <div class="text-body1">
+          成功高中代聯會分為行政部門及班代大會，行政部門又分為學權、活動、公關、美宣、總務及秘書六個常設組，
+          任內辦理各式活動、講座，也會向學校爭取權益並參與校內重大會議。
+          班代大會負責監督制衡行政部門，其權責為審理預算、制定法案、表決提案。
+        </div>
+      </q-card-section>
+
+      <q-separator spaced />
+
+      <q-card-section class="q-pa-md text-center">
+        <div class="text-h6 q-mb-md"><strong>聯絡代聯會</strong></div>
+        <q-btn flat round size="xl" color="grey-2" class="q-pa-none" @click="goToInstagram">
+          <q-img
+            src="/icons/instagram.svg"
+            alt="Instagram"
+            style="width: 100px; height: 100px"
+            fit="contain"
+          />
+        </q-btn>
+        <div class="text-subtitle1 q-mt-sm"><strong>Instagram</strong></div>
+      </q-card-section>
+    </q-card>
   </q-page>
 </template>
 
-<script setup lang="ts">
-import { ref } from 'vue';
-import type { Todo, Meta } from 'components/models';
-import ExampleComponent from 'components/ExampleComponent.vue';
-
-const todos = ref<Todo[]>([
-  {
-    id: 1,
-    content: 'ct1'
+<script lang="ts">
+export default {
+  methods: {
+    goToInstagram() {
+      window.open('https://www.instagram.com/cgcra69_official/', '_blank');
+    },
   },
-  {
-    id: 2,
-    content: 'ct2'
-  },
-  {
-    id: 3,
-    content: 'ct3'
-  },
-  {
-    id: 4,
-    content: 'ct4'
-  },
-  {
-    id: 5,
-    content: 'ct5'
-  }
-]);
-
-const meta = ref<Meta>({
-  totalCount: 1200
-});
+};
 </script>
+
+<style scoped></style>
